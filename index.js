a196775c2f9936f86aa22cbeb87afd289cf8c574
Sachini Ranasingha <sachiniranasingha595@gmail.com>
2024-11-14 14:06:49 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,83 @@
+$(document).ready(function(e){
+    $win = $(window);
+    $navbar = $('#header');
+    $toggle = $('.toggle-button');
+    var width = $navbar.width();
+    toggle_onclick($win, $navbar, width);
+
+    // resize event
+    $win.resize(function(){
+        toggle_onclick($win, $navbar, width);
+    });
+
+    $toggle.click(function(e){
+        $navbar.toggleClass("toggle-left");
+    })
+
+});
+
+
+$(document).ready(function($){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#topBtn').fadeIn('slow');
+        } else {
+            $('#topBtn').fadeOut('slow');
+        }
+    });
+    $('#topBtn').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+});
+
+$(window).on("load",function(){
+    $(".loader-container").fadeOut(3000);
+});
+
+
+
+
+ 
+
+function toggle_onclick($win, $navbar, width){
+    if($win.width() <= 768){
+        $navbar.css({left: `-${width}px`});
+    }else{
+        $navbar.css({left: '0px'});
+    }
+}
+
+var typed = new Typed('#typed' , {
+    strings: [
+        'Student...',
+        'Web Developer...',
+        'Freelancer...',
+        'Graphic Designer'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+var typed_2 = new Typed('#typed_2' , {
+    strings: [
+        'Student...',
+        'Web Developer...',
+        'Freelancer...',
+        'Graphic Designer'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+    anchor.addEventListener('click', function(e){
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
